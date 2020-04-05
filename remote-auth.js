63d1a9bab6a557b4b3a0f83f2d63d6dc7aee823d
Sam Kavanagh <hganavak@gmail.com>
2020-04-06 10:26:48 +1200

    Removed auth stuff and tidied

--- remote-auth.js
+++ remote-auth.js
@@ -1,86 +1,11 @@
 const { ApolloServer, gql } = require('apollo-server');
-const { AuthDirective } = require('./auth-directive');
 const { introspectSchema, makeExecutableSchema, makeRemoteExecutableSchema, mergeSchemas, AuthenticationError } = require('graphql-tools');
 const { HttpLink } = require('apollo-link-http');
 const fetch = require('node-fetch');
 
-const articles = [
-  {
-    body: 'Gonna teach you some stuff',
-    isPublic: true
-  },
-  {
-    body: 'This is all top secret',
-    isPublic: false
-  },
-];
-
-const contentItems = [
-    {
-        title: 'Sams Latex Workshop',
-        summary: 'A workshop about LaTeX',
-        article: articles[0]
-    },
-    {
-        title: 'Top Secret NASA Project',
-        summary: 'A top secret article about NASA',
-        article: articles[1]
-    }
-]
-
-
-// Define schema (collection of type definitions)
-const typeDefs = gql`
-
-    directive @auth(
-        requires: Role = USER,
-    ) on OBJECT
-
-    enum Role {
-        USER
-        STAFF
-    }
-
-    type ContentItem {
-        title: String
-        summary: String
-        article: Article
-    }
-
-    type Article @auth {
-        body: String 
-        isPublic: Boolean
-    }
-
-    type Query {
-        contentItems: [ContentItem]
-        articles: [Article]
-    }
-
-`;
-
-// Define resolvers (define the technique for fetching the types defined in our schema)
-const resolvers = {
-    Query: {
-        contentItems: (parent, args, context) => { return contentItems },
-        articles: (parent, args, context) => { return articles }
-    },
-};
-
-// The ApolloServer constructor requires two parameters: your schema
-// definition and your set of resolvers.
-const localSchema = makeExecutableSchema({
-    typeDefs,
-    resolvers,
-    schemaDirectives: {
-        auth: AuthDirective
-    }
-});
-
-
 // Set up remote schemas
 // Load a remote schema and set up the http-link
-getRemoteSchema = async(remoteUri) => {
+getRemoteSchema = async(remoteUri, headers) => {
     try {
         console.log('Loading remote schema:', remoteUri)
         const link = new HttpLink({ uri: remoteUri, fetch });
@@ -100,21 +25,19 @@ getRemoteSchema = async(remoteUri) => {
 initialize = async () => {
 
     // Load remote schemas here
-    // spacexSchema = await getRemoteSchema('https://api.spacex.land/graphql');
     countriesSchema = await getRemoteSchema('https://countries.trevorblades.com/');
 
     // Merge all schemas (remote and local) here
     const schema = mergeSchemas({
         schemas: [
-            // spacexSchema,
             countriesSchema,
-            localSchema
         ],
         mergeDirectives: true,
         resolvers: [
                 custom => { return { Query: {
                     countries: (root, args, context, info) => {
-                        console.log(context)
+
+                        // If the user is signed in, simply forward request
                         if(context.user) {
                             return info.mergeInfo.delegateToSchema({
                                 schema: countriesSchema,
@@ -124,8 +47,10 @@ initialize = async () => {
                                 context,
                                 info
                             });
-                        } else {
-                            console.log('not auth')
+                        } 
+
+                        // If the user is not signed, forward request with additional filtering arguments
+                        else {
                             return info.mergeInfo.delegateToSchema({
                                 schema: countriesSchema,
                                 operation: 'query',
